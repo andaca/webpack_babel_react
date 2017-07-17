eab62456632ec2af274c5d7d920525d38e8d5115
Andrew Cameron <andrew.cameron@protonmail.com>
2017-07-17 19:06:03 +0100

    clock

--- js/client.js
+++ js/client.js
@@ -1,16 +1,68 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class Layout extends React.Component {
-    render() {
+const user = {
+    name: 'Andy',
+    nickname: 'The mattador!'
+}
+
+const Welcome = (props) => <h1> Hello {props.name} </h1>
+const Paragraph = (props) => <p> {props.text} </p>
+const Footer = (props) => <p> {props.text} </p>
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { date: new Date() }
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(), 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID)
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
+
+    render() { 
         return (
-            <h1>
-                React is working :)
-            </h1>
+            <div>
+                <h1>Tick, tock!</h1>
+                <h2>{this.state.date.toLocaleTimeString()}</h2>
+            </div>
         )
     }
 }
 
-const app = document.getElementById('app')
-ReactDOM.render(
-    <Layout/>, app)
+const Link = () => {
+    const handleClick = (e) => {
+        e.preventDefault()
+        alert('hello!')
+    }
+
+    return (
+        <a onClick={handleClick}>
+            click me!
+        </a>
+    )
+}
+
+const App = () => (
+    <div>
+        <Welcome name={user.name} />
+        <Paragraph text="This is the paragraph text" />
+        <Clock />
+        <Footer text="Goodbye!" />
+        <Link />
+    </div>
+)
+
+const root = document.getElementById('root')
+ReactDOM.render(<App />, root)
